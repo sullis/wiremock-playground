ba206d37055b952477f63883bc8d83913c86576e
sullis <github@seansullivan.com>
2024-05-15 17:45:34 -0700

    WiremockTest helloWorld

--- src/test/java/io/github/sullis/playground/wiremock/WiremockTest.java
+++ src/test/java/io/github/sullis/playground/wiremock/WiremockTest.java
@@ -1,11 +1,33 @@
 package io.github.sullis.playground.wiremock;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+
 
 
 public class WiremockTest {
+  @RegisterExtension
+  static WireMockExtension wiremock = WireMockExtension.newInstance()
+      .options(wireMockConfig().dynamicPort())
+      .build();
+
   @Test
-  void helloWorld() {
-    // TODO
+  void helloWorld() throws Exception {
+    wiremock.stubFor(get("/").willReturn(ok()));
+    HttpClient httpclient = HttpClient.newHttpClient();
+    URI uri = URI.create(wiremock.url("/"));
+    HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+    HttpResponse response = httpclient.send(request, HttpResponse.BodyHandlers.ofString());
+    assertThat(response.statusCode()).isEqualTo(200);
   }
 }
